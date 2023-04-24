ee5a7dcac3bef445896cbd09adb86f85074e7635
Nguyen Van Man <vanman12315@gmail.com>
2023-04-24 15:29:43 +0700

    HOMEPAGE

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
-function formSubmit() {
-    alert("Cảm ơn bạn đã liên hệ với chúng tôi. Chúng tôi sẽ liên lạc lại trong thời gian sớm nhất.");
-  }
+let subMenuShow = false;
+
+const subMenuButton  = () => {
   
+  const submenu = document.getElementById('sub-menu');
+  if(!subMenuShow){
+    submenu.classList.remove('hide-element');
+  }
+  else{
+    submenu.classList.add('hide-element');
+
+  }
+  subMenuShow = !subMenuShow;
+}
